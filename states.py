ff2b3f8df5a671a5a68dafabf19395737359eb13
SHNUYK <99737441+SHNUYK@users.noreply.github.com>
2026-02-01 17:03:31 +0300

    Update states.py

--- states.py
+++ states.py
@@ -1,8 +1,8 @@
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
 
-class CharacterCreation(StatesGroup):
-    waiting_for_xp = State()
-    choosing_homeworld = State()
-    rolling_characteristics = State()
-    assigning_characteristics = State()
+class Creation(StatesGroup):
+    choosing_world = State()
+    choosing_background = State()
+    choosing_role = State()
+    finished = State()
