cba0ebbe4973a2cca2d8086078deab09d39a994a
SHNUYK <99737441+SHNUYK@users.noreply.github.com>
2026-02-01 16:34:03 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,13 @@
+import os
+import secrets
+from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.utils import executor
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot, storage=MemoryStorage())
